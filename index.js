69ba8ea301aa9c64e28bf114b49c2616b3b6bc46
nikita <nikitabadani@gmail.com>
2026-02-20 23:45:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+const app=express();
+const port=3000;
+const token="7feec297-af9e-4d12-a634-a84a78026ab7"
+const config={
+    headers:{Authorization: `Bearer ${token}`}
+}
+
+app.use(express.static("public"))
+
+app.get("/",async(req,res)=>{
+    try{
+    const result=await axios.get("https://secrets-api.appbrewery.com/random",config)
+    const response=result.data
+    res.render("index.ejs",{
+        secret: response.secret,
+        user: response.username
+    })}
+    catch(error){
+        res.status(404).send(error.message)
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`app is running on port ${port}`)
+})
+
